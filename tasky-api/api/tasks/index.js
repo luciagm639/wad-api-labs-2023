1f377b967aa5765176ed5c618e4b5af94da59dfb
Lucia <luciagm639@gmail.com>
2023-11-17 13:59:58 +0100

    Week 9 Lab exercise done

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -21,13 +21,17 @@ router.get('/:id', (req, res) => {
 //Add a task
 router.post('/', (req, res) => {
     const { title, description, deadline, priority, done } = req.body;
+    const created_at = new Date().toISOString();
+    const updated_at = new Date().toISOString();
     const newTask = {
         id: uuidv4(),
         title,
         description,
         deadline,
         priority,
-        done
+        done,
+        created_at,
+        updated_at
     };
     tasksData.tasks.push(newTask);
     res.status(201).json(newTask);
@@ -42,6 +46,7 @@ router.put('/:id', (req, res) => {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
     const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id };
+    updatedTask.updated_at = new Date().toISOString();
     tasksData.tasks[taskIndex] = updatedTask;
     res.json(updatedTask);
 });
