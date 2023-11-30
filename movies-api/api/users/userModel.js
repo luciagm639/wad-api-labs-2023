60001570cf694102c384494b3f027955ec125751
Lucía Gutiérrez Molina <luciagutierrezmolina22@gmail.com>
2023-11-30 18:15:34 +0100

    Initialise Dev Env - Data

---
+++ movies-api/api/users/userModel.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, unique: true, required: true},
+  password: {type: String, required: true }
+});
+
+UserSchema.path("password").validate(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/);
+
+export default mongoose.model('User', UserSchema);
